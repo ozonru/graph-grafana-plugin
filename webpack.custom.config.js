7ef6cd3bc67d73f81bb298e2d90d351ffe6e7c55
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-08-12 16:09:33 +0300

    custom build configuration

---
+++ webpack.custom.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const {
+  loadWebpackConfig
+} = require("@grafana/toolkit/src/config/webpack.plugin.config");
+
+module.exports = async (envs) => {
+  const isProduction = !envs.development;
+  const watch = !!envs.watch;
+
+  debugger;
+  const config = await loadWebpackConfig({
+    production: isProduction,
+    watch
+  });
+
+  // define "grafana/**" as non external dep
+  config.externals = config.externals.slice(0, config.externals.length - 1);
+
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...(config.resolve ? config.resolve.alias : undefined),
+      ["@grafana/ui"]: path.resolve(__dirname, "node_modules/grafana", "packages/grafana-ui")
+    }
+  };
+
+  return config;
+};
